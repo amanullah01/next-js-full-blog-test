cedc184ee43ca91c59ba36eb6f76bd1b3d0ebc33
Amanullah Aman <amancse.aiub@gmail.com>
2023-09-07 01:01:37 +0600

    rendering markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,21 +2,15 @@ import ReactMarkdown from "react-markdown";
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
 
-const DUMMY_POSTS = {
-  slug: "getting-started-with-next-js-1",
-  title: "Getting Started With Next Js",
-  date: "2023-06-29",
-  image: "getting-started-with-next-js.png",
-  content: "# This is a first post",
-};
-
 const PostContent = (props) => {
-  const imagePath = `/images/posts/${DUMMY_POSTS.slug}/${DUMMY_POSTS.image}`;
+  const { post } = props;
+  console.log(post);
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POSTS.title} image={imagePath} />
+      <PostHeader title={post.title} image={imagePath} />
       {/* {DUMMY_POSTS.content} */}
-      <ReactMarkdown>{DUMMY_POSTS.content}</ReactMarkdown>
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 };
--- pages/posts/[...slug].js
+++ pages/posts/[...slug].js
@@ -1,7 +1,25 @@
 import PostContent from "@/components/posts/post-detail/post-content";
+import { getPostData, getPostsFiles } from "@/lib/posts-util";
 
-const PostDetails = () => {
-  return <PostContent />;
+const PostDetails = (props) => {
+  return <PostContent post={props.post} />;
+};
+
+export const getStaticProps = (context) => {
+  const { params } = context;
+  const { slug } = params;
+  const postData = getPostData(slug);
+  return { props: { post: postData }, revalidate: 600 };
+};
+
+export const getStaticPaths = () => {
+  const postFiles = getPostsFiles();
+  const slugs = postFiles.map((fileName) => fileName.replace(/\.md$/, ""));
+  return {
+    // paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    paths: [],
+    fallback: "blocking",
+  };
 };
 
 export default PostDetails;
