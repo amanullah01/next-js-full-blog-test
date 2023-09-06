23b33f2de28d092ef3fb6e5dfb442481f7d2d2b4
Amanullah Aman <amancse.aiub@gmail.com>
2023-09-07 02:44:40 +0600

    Head added

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,17 @@
 import ContactForm from "@/components/contact/contact-form";
+import Head from "next/head";
+import { Fragment } from "react";
 
 const ConatactPage = () => {
-  return <ContactForm />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Contact Form</title>
+        <meta name="description" content="Send me message" />
+      </Head>
+      <ContactForm />
+    </Fragment>
+  );
 };
 
 export default ConatactPage;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,8 +1,21 @@
 import AllPosts from "@/components/posts/all-posts";
 import { getAllPosts } from "@/lib/posts-util";
+import Head from "next/head";
+import { Fragment } from "react";
 
 const AllPostsPage = (props) => {
-  return <AllPosts posts={props.posts} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>All Blog Posts</title>
+        <meta
+          name="description"
+          content="All programming related blog posts are here"
+        />
+      </Head>
+      <AllPosts posts={props.posts} />
+    </Fragment>
+  );
 };
 
 export default AllPostsPage;
