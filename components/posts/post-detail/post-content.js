0db5ff214c51b1b6af3f2e5bce15dbe5570e931d
Amanullah Aman <amancse.aiub@gmail.com>
2023-09-07 03:16:09 +0600

    code optimize

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,8 +2,13 @@ import ReactMarkdown from "react-markdown";
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
 import Image from "next/image";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import {atomDark} from 'react-syntax-highlighter/dist/cjs/styles/prism'
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
+import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
+import css from "react-syntax-highlighter/dist/cjs/languages/prism/css";
+
+SyntaxHighlighter.registerLanguage("js", js);
+SyntaxHighlighter.registerLanguage("css", css);
 
 const PostContent = (props) => {
   const { post } = props;
@@ -39,9 +44,9 @@ const PostContent = (props) => {
       return <p>{paragraph.children}</p>;
     },
 
-    code(code){
+    code(code) {
       const { className, children } = code;
-      const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+      const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
       return (
         <SyntaxHighlighter
           style={atomDark}
@@ -49,7 +54,7 @@ const PostContent = (props) => {
           children={children}
         ></SyntaxHighlighter>
       );
-    }
+    },
   };
 
   return (
