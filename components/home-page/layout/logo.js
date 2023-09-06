05255d201a4c07db373f2402dfe6bb8749ecfdfc
Amanullah Aman <amancse.aiub@gmail.com>
2023-09-06 16:50:11 +0600

    logo

---
+++ components/home-page/layout/logo.js
@@ -0,0 +1,8 @@
+import classes from "./logo.module.css";
+import { Fragment } from "react";
+
+const Logo = (props) => {
+  return <div className={classes.logo}>Next Blog</div>;
+};
+
+export default Logo;
