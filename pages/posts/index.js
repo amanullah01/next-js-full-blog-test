196c37f7fd84c755277febfa07e9da3a017df3b0
Amanullah Aman <amancse.aiub@gmail.com>
2023-09-06 23:46:50 +0600

    show all posts

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,38 @@
-const PostsPage = () => {
-  return <h1>Posts page</h1>;
+import AllPosts from "@/components/posts/all-posts";
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-next-js-1",
+    title: "Getting Started With Next Js",
+    date: "2023-06-29",
+    image: "getting-started-with-next-js.png",
+    excerpt: "NextJs is the React framework for production.",
+  },
+  {
+    slug: "getting-started-with-next-js-2",
+    title: "Getting Started With Next Js2",
+    date: "2023-06-29",
+    image: "getting-started-with-next-js.png",
+    excerpt: "NextJs is the React framework for production.",
+  },
+  {
+    slug: "getting-started-with-next-js-3",
+    title: "Getting Started With Next Js",
+    date: "2023-06-29",
+    image: "getting-started-with-next-js.png",
+    excerpt: "NextJs is the React framework for production.",
+  },
+  {
+    slug: "getting-started-with-next-js-4",
+    title: "Getting Started With Next Js",
+    date: "2023-06-29",
+    image: "getting-started-with-next-js.png",
+    excerpt: "NextJs is the React framework for production.",
+  },
+];
+
+const AllPostsPage = () => {
+  return <AllPosts posts={DUMMY_POSTS} />;
 };
 
-export default PostsPage;
+export default AllPostsPage;
